7c021c7fc05c19041e41a6c683bfb16d43bb0214
Fabrizio Arias <fabriidev@gmail.com>
2024-10-23 20:16:42 -0300

    carga de turnos
    Carga de turnos de manera dinámica en la ventana principal... Según la fecha de hoy

--- controller/turnoController.js
+++ controller/turnoController.js
@@ -11,16 +11,14 @@ class TurnoController {
             let fecha = req.body.fecha
             let turnos = await turnoModel.turnosPorDia(fecha);
             let jsonstring = JSON.stringify(turnos)
-            console.log(jsonstring)
-            
-
             if (!fecha ) {
-                return res.status(400).json({ succes: false, error: 'la fecha debe ser seleccionada' })
-            }else if (turnos==null) {
-                return res.status(400).json({ succes: false, error: 'no se encontraron turnos' })
+                return res.status(400).json({ success: false, error: 'la fecha debe ser seleccionada' })
+            }
+            if (turnos == null) {
+                return res.status(400).json({ success: false, error: 'no se encontraron turnos' })
             }
 
-            return res.status(200).send(jsonstring)
+            return res.status(200).send({success: true, jsonstring})
         } catch (error) {
             console.log('Error al pintar turnos: ', error);
             res.status(500).json({ success: false, error: 'Error interno del servidor' });
--- public/js/agendaCliente.js
+++ public/js/agendaCliente.js
@@ -2,14 +2,11 @@ const intputFecha = document.getElementById('fecha');
 let hoy = new Date;
 let fechaSeleccionada = hoy.toISOString().split('T')[0];
 
+let pintarTablaTurnos = document.getElementById('pintarTablaTurnos');
 
 document.getElementById('fechaInicial').innerHTML = fechaSeleccionada;
 
-
-
-intputFecha.addEventListener('change', (event) => {
-    fechaSeleccionada = event.target.value;
-    console.log(fechaSeleccionada);
+function turnosHoy() {
     fetch('/turnos/pintarTurnos', {
         method: 'POST',
         headers: {
@@ -26,7 +23,44 @@ intputFecha.addEventListener('change', (event) => {
         return response.json();
     })
     .then(data => {
-        console.log(data)
+        //console.log(data)
+        if (data.success) {
+            
+            const turnos = JSON.parse(data.jsonstring);
+            console.log(turnos);
+
+            for (const element of turnos) {
+                document.getElementById('pintarTablaTurnos').innerHTML += pintarTabla(element);
+            }
+
+            // document.getElementById('pintarTablaTurnos').innerHTML = pintarTabla(element);
+        } else {
+            console.error(data.message || 'No se encontraron turnos');
+        }
+    })
+    .catch(error => {
+        console.error('Error al obtener turnos:', error);
+    });
+}
+turnosHoy();
+
+intputFecha.addEventListener('change', (event) => {
+    fechaSeleccionada = event.target.value;
+    console.log(fechaSeleccionada);
+    fetch('/turnos/pintarTurnos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            fecha: fechaSeleccionada
+        })
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        //console.log(data.success)
         if (data.success) {
             const turnos = data.turnos; // Asegúrate de acceder a los turnos correctamente
             console.log(turnos);
@@ -41,3 +75,26 @@ intputFecha.addEventListener('change', (event) => {
 });
 
 
+function pintarTabla(turnos){
+    let colorEstado;
+    if(turnos.estado === 'Cancelado'){
+        colorEstado = 'bg-danger';
+    }else if(turnos.estado === 'Atendido'){
+        colorEstado = 'bg-success';
+    }else{
+        colorEstado = 'bg-primary';
+    }
+
+
+    pintarTablaTurnos = `
+            <tr>
+                <td>
+                    <a href="#" target="_blank"> ${turnos.apellido} ${turnos.nombre} </a>
+                </td>
+                <td> ${turnos.hora} </td>
+                <td> ${turnos.motivo_consulta} </td>
+                <td class="${colorEstado}"> ${turnos.estado} </td>
+            </tr>`;
+
+    return pintarTablaTurnos;
+}
