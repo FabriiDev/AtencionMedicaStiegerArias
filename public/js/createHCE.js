3afe6c0c964c128e835b59b55aa51bcd548beb98
Valentin Stieger <50178370+StiegerV@users.noreply.github.com>
2024-11-05 16:13:50 -0300

    logica de creacion hce

--- public/js/createHCE.js
+++ public/js/createHCE.js
@@ -1,44 +1,106 @@
 
-function diagnostico() {
-    console.log('diag')
-    document.getElementById('diagnosticoDetalle').disabled=false
-    document.getElementById('estadoDiagnostico').disabled=false
+var historial = {
 
-    
+    diagnosticos: [],
+
+    evolucion: '',
+
+    medicamentos: [],
+
+    antecedentes: [],
+
+    habitos:[],
+
+    alergias: [],
 }
 
-function evolucion() {
-    console.log('evo')
-    document.getElementById('evolucionDetalle').disabled=false
-    
+
+
+function guardarDiagnostico() {
+    console.log('diagnosticoGuardar')
+    let detalle = document.getElementById('diagnosticoDetalle').value
+    let estado = document.getElementById('estadoDiagnostico').value
+    let data = { detalle: detalle, estado: estado }
+    historial.diagnosticos.push(data)
+
+
 }
+
+
+
+function guardarEvolucion() {
+    console.log('evoguardar')
+    let detalle = document.getElementById('evolucionDetalle').value
+
+
+
+
+    //disabled= true al terminar
+}
+
 function medicamento() {
     console.log('mesi')
-    document.getElementById('nombreMedicamento').disabled=false
-    document.getElementById('detalleMedicamento').disabled=false
+    document.getElementById('nombreMedicamento').disabled = false
+    document.getElementById('detalleMedicamento').disabled = false
+}
+
+function guardarMedicamento() {
+    console.log('mesiGuardar')
+    let nombre = document.getElementById('nombreMedicamento').value
+    let detalle = document.getElementById('detalleMedicamento').value
+    let data = { nombre: nombre, detalle: detalle }
+    historial.medicamentos.push(data)
 }
 
 function antecedentes() {
     console.log('ante')
-    document.getElementById('detalleAntecedentes').disabled=false
-    document.getElementById('desdeAntecedentes').disabled=false
-    document.getElementById('hastaAntecedentes').disabled=false
+    document.getElementById('detalleAntecedentes').disabled = false
+    document.getElementById('desdeAntecedentes').disabled = false
+    document.getElementById('hastaAntecedentes').disabled = false
+}
+
+function guardarAntecedentes() {
+    console.log('antesGuardar')
+    let detalle = document.getElementById('detalleAntecedentes').value
+    let fdesde = document.getElementById('desdeAntecedentes').value
+    let fhasta = document.getElementById('hastaAntecedentes').value
+    let data={detalle:detalle,fdesde:fdesde,fhasta:fhasta}
+    historial.antecedentes.push(data)
 }
 
 
 function habitos() {
     console.log('habi')
-    document.getElementById('detalleHabitos').disabled=false
-    document.getElementById('desdeHabitos').disabled=false
-    document.getElementById('hastaHabitos').disabled=false
+    document.getElementById('detalleHabitos').disabled = false
+    document.getElementById('desdeHabitos').disabled = false
+    document.getElementById('hastaHabitos').disabled = false
+}
+
+function guardarHabitos() {
+    console.log('habitoGuardar')
+    let detalle = document.getElementById('detalleHabitos').value
+    let fdesde = document.getElementById('desdeHabitos').value
+    let fhasta = document.getElementById('hastaHabitos').value
+    let data={detalle:detalle,fdesde:fdesde,fhasta:fhasta}
+    historial.habitos.push(data)
 }
 
 function alergia() {
     console.log('alergia')
-    document.getElementById('nombreAlergia').disabled=false
-    document.getElementById('desdeAlergia').disabled=false
-    document.getElementById('hastaAlergia').disabled=false
-    document.getElementById('detalleAlergia').disabled=false
+    document.getElementById('nombreAlergia').disabled = false
+    document.getElementById('desdeAlergia').disabled = false
+    document.getElementById('hastaAlergia').disabled = false
+    document.getElementById('detalleAlergia').disabled = false
+}
+
+function guardarAlergia() {
+    console.log('alergiaGuardar')
+    let nombre = document.getElementById('nombreAlergia').value
+    let fdesde = document.getElementById('desdeAlergia').value
+    let fhasta = document.getElementById('hastaAlergia').value
+    let detalle = document.getElementById('detalleAlergia').value
+    let data={nombre:nombre,fdesde:fdesde,fhasta:fhasta,detalle}
+    historial.alergias.push(data)
 }
 
 
